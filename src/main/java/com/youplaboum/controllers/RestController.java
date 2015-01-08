d80ea4e69c532a824d6d9ff46836745918c304b5
tlerue <tlerue@gmail.com>
2015-01-08 13:09:05 +0100

    first

---
+++ src/main/java/com/youplaboum/controllers/RestController.java
@@ -0,0 +1,31 @@
+package com.youplaboum.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.youplaboum.models.User;
+import com.youplaboum.services.DataService;
+
+/**
+ * @author Tristan.LR
+ *
+ */
+@Controller
+@RequestMapping("/api/users")
+public class RestController {
+
+	@Autowired
+	DataService dataService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public List<User> list() {
+		return dataService.getUserList();
+
+	}
+}
---
+++ src/main/java/com/youplaboum/services/DataService.java
@@ -0,0 +1,12 @@
+package com.youplaboum.services;
+
+import java.util.List;
+
+import com.youplaboum.models.User;
+/**
+ * @author Tristan LR
+ *
+ */
+public interface DataService {
+	public List<User> getUserList();
+}
